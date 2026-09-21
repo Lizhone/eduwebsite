54dd248ff68d15a67fdbdb2f32ab2d2095b398b2
Lizhone <lizhoneremeh@gmail.com>
2026-09-21 15:42:32 +0530

    Initial commit

---
+++ form-handler.php
@@ -0,0 +1,21 @@
+<?php
+$name=$_POST['name'];
+$visiotor_email=$_POST['email'];
+$subject=$_POST['subject'];
+$message=$_POST['message'];
+?>
+$email_form ='eduwebsite.com';
+
+$email_subject='New Form Submission';
+$email_body="User Name:$name.\n".
+             "User Email: $visitor_email.\n".
+             "Subject:$subject.\n".
+             "User Message:$message .\n";
+$to ='lizho7466@gmail.com';
+
+$header = "From: $email_from \r\n";
+
+$header .="Reply-To:$visitor_email \r\n";
+
+mail($to, $email_subject, $email_body,$header);
+header("Location:contact.html");
